9ed3ec9cda6dc569c5e6731fa333f5926ff7ba41
egorka44252 <144693055+egorka44252@users.noreply.github.com>
2026-08-01 13:12:23 +0300

    Add files via upload

---
+++ scripts/myscript.lua
@@ -0,0 +1,8 @@
+{
+  "name": "My Script",
+  "desc": "Описание",
+  "author": "You",
+  "category": "Misc",
+  "version": "1.0",
+  "url": "https://raw.githubusercontent.com/ТВОЙ_НИК/xys-scripts/main/scripts/myscript.lua"
+}
