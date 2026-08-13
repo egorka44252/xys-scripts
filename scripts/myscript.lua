45f6802b9c02d3f54ee5961573e0107e1f4ad448
egorka44252 <144693055+egorka44252@users.noreply.github.com>
2026-08-14 00:46:34 +0300

    Delete scripts directory

--- scripts/myscript.lua
+++
@@ -1,8 +0,0 @@
-{
-  "name": "My Script",
-  "desc": "Описание",
-  "author": "You",
-  "category": "Misc",
-  "version": "1.0",
-  "url": "https://raw.githubusercontent.com/ТВОЙ_НИК/xys-scripts/main/scripts/myscript.lua"
-}
